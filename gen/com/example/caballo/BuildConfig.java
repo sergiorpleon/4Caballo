1edebbf2c26caf69c3ccb0d2c3b66d8fea9c3e9a
sergiopleon <sergiopleon@gmail.com>
2018-05-10 09:54:20 -0400

    Salva inicial de mi juego 4Caballos

---
+++ gen/com/example/caballo/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.caballo;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/example/caballo/help.java
@@ -0,0 +1,14 @@
+package com.example.caballo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class help extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_help);
+	}
+	
+}
